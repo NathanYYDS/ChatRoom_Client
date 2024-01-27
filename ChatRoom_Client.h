91540fe49f843908acbf579b165661f09c1bd748
钟果辰 <11775492+guoziyyds@user.noreply.gitee.com>
2024-01-27 21:35:11 +0800

    添加项目文件。

---
+++ ChatRoom_Client.h
@@ -0,0 +1,32 @@
+﻿
+// ChatRoom_Client.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CChatRoomClientApp:
+// 有关此类的实现，请参阅 ChatRoom_Client.cpp
+//
+
+class CChatRoomClientApp : public CWinApp
+{
+public:
+	CChatRoomClientApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CChatRoomClientApp theApp;
---
+++ resource.h
@@ -0,0 +1,39 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 ChatRoomClient.rc 使用
+//
+#define IDD_CHATROOM_CLIENT_DIALOG      102
+#define IDP_SOCKETS_INIT_FAILED         103
+#define IDR_MAINFRAME                   128
+#define IDD_SERVER_SETTING_DIALOG       130
+#define IDD_SIGNUP_DIALOG               132
+#define IDC_TEXT_SEND                   1000
+#define ID_CONNECT_TEST                 1001
+#define IDC_SEND                        1001
+#define ID_CONNECT_APPLY                1002
+#define IDC_LIST1                       1002
+#define IDC_IPADDRESS1                  1003
+#define IDC_SERVER_SETTING              1003
+#define IDC_EDIT1                       1004
+#define IDC_CONNECT_STATUS              1004
+#define IDC_EDIT2                       1005
+#define IDC_SIGNIN                      1006
+#define IDC_EDIT3                       1007
+#define IDC_SIGNUP_PASSWORD2            1008
+#define IDC_SIGNUP                      1008
+#define IDC_SIGNOUT                     1009
+#define ID_SIGNUP_CONFIRM               1010
+#define IDC_SIGNUP_USERNAME             1011
+#define IDC_SIGNUP_PASSWORD1            1012
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
